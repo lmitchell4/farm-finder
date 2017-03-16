6eacda02d5d4f741be8cf5d4b43a6830df3e71ad
lmitchell4 <lmitchell4@gmail.com>
2017-03-15 18:20:06 -0700

    Fix Blueprint import statements

--- catalog/__init__.py
+++ catalog/__init__.py
@@ -9,12 +9,10 @@ from .views.index import index
 from .views.login import login
 from .views.logout import logout
 from .views.farm import farm
-from .views.catalog import catalog_show, catalog_manage, catalog_new
-from .views.catalog import catalog_edit, catalog_delete
+from .views.catalog import catalog
+from .views.profile import profile
+from .views.event import event
 from .views.error import error
-from .views.event import event_show, event_manage, event_new
-from .views.event import event_delete, event_edit
-from .views.profile import profile_show, profile_manage, profile_edit
 from .apis import apis
 
 ############################################################################
@@ -30,47 +28,11 @@ app.config["UPLOAD_FOLDER_ITEM"] = "./catalog/static/userImages/item"
 app.config["ALLOWED_EXTENSIONS"] = set(["pdf", "png", "jpg", "jpeg", "gif"])
 
 app.register_blueprint(index)
-
 app.register_blueprint(login)
 app.register_blueprint(logout)
-
-app.register_blueprint(farm_show_all)
-app.register_blueprint(farm_manage)
-app.register_blueprint(farm_delete)
-app.register_blueprint(farm_new)
-
+app.register_blueprint(farm)
 app.register_blueprint(catalog)
-
-app.register_blueprint(error)
-
-app.register_blueprint(event)
-
 app.register_blueprint(profile)
-
+app.register_blueprint(event)
+app.register_blueprint(error)
 app.register_blueprint(apis)
-
-
-
-
-# import catalog.views.index
-# import catalog.views.login
-# import catalog.views.logout
-# import catalog.views.farm_show_all
-# import catalog.views.farm_show_user
-# import catalog.views.farm_edit
-# import catalog.views.farm_delete
-# import catalog.views.farm_new
-# import catalog.views.catalog_show
-# import catalog.views.catalog_manage
-# import catalog.views.catalog_edit
-# import catalog.views.catalog_delete
-# import catalog.views.catalog_new
-# import catalog.views.profile_show
-# import catalog.views.profile_manage
-# import catalog.views.profile_edit
-# import catalog.views.event_show
-# import catalog.views.event_manage
-# import catalog.views.event_edit
-# import catalog.views.event_delete
-# import catalog.views.event_new
-# import catalog.apis
