9581ccb432fe7295097e6a1e69b323e190f2ca6e
lmitchell4 <lmitchell4@gmail.com>
2017-06-20 16:56:16 -0700

    chore: Remove some old TO-DO's

--- application.py
+++ application.py
@@ -11,9 +11,3 @@ if __name__ == "__main__":
   application.secret_key = "super_secret_key"
   application.debug = True
   application.run(host = "0.0.0.0", port = 5000)
-
-
-
-# To do: fix oauth2client library problem
-# To do: fix broken file structure. It was working before; did some thing 
-#         about flask change?
